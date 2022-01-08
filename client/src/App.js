09b5e45a92bca485473f461c949b7812c1b54709
Hayden Lugo <hayden@lugowebdevelopment.com>
2022-01-08 18:33:02 +0100

    Update

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Body from './Body'
+import Nav from './pages/components/Nav';
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Body />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,8 @@
+
+  
+import { gql } from '@apollo/client';
+
+//basic setup
+// export const NAME = gql`
+// mutation
+// `;
