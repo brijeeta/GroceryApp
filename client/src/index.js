343fb2cee140f59e72109f2a85710c3b1d09d364
Holy Paul <brijeeta@gmail.com>
2022-01-09 16:03:06 -0500

    added login,signup, navbar components, edited user.js, added client and server utils,user mutations

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
-
+import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,20 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
-import App from './App';
-import ReactDOM from 'react-dom';
-//import css
-
-
-// ReactDOM.render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>
-//     document.getElementById('') <-- this id can be found in public/index.html
-// );
--- client/src/utils/queries.js
+++
@@ -1,6 +0,0 @@
-import { gql } from '@apollo/client';
-
-//basic setup
-// export const NAME = gql`
-//     query 
-// `;
