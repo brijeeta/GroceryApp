9e18be733a32ef543cc4b6ae4439eac401f6db59
Holy Paul <brijeeta@gmail.com>
2022-01-11 17:00:05 -0500

    trying to fix the deployment error

--- server/server.js
+++ server/server.js
@@ -18,8 +18,8 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.allowedNodeEnvironmentFlags.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/public')));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('*', (req, res) => {
