fc6ca61657d2cd778afd57f5412690ba032ba71d
Holy Paul <brijeeta@gmail.com>
2022-01-11 21:03:43 -0500

    fixed login/signup BE functionality, added resolvers for users, typdefs moduified,added comments in user model

--- server/models/User.js
+++ server/models/User.js
@@ -23,6 +23,7 @@ const userSchema = new Schema({
     lists: [List.schema]
 });
 
+// set up pre-save middleware to create password
 userSchema.pre('save', async function (next) {
     if (this.isNew || this.isModified('password')) {
         const saltRounds = 10;
@@ -32,6 +33,7 @@ userSchema.pre('save', async function (next) {
     next();
 });
 
+// compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function (password) {
     return await bcrypt.compare(password, this.password);
 };
