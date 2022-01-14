5420bf5a118302728c37139de1f85df70988f2e3
Kyle Lux <kyleslux86@gmail.com>
2022-01-13 21:55:33 -0500

    minor changes to function structure

--- server/utils/KrogerAPI.js
+++ server/utils/KrogerAPI.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+const axios = require ('axios');
 
 const { KROGER_CLIENT_ID, KROGER_CLIENT_SECRET } = process.env;
 
-const krogerAuth = () => {
+const krogerAuth = async () => {
     const name = 'Kroger_Token';
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
@@ -21,30 +21,31 @@ const krogerAuth = () => {
 
     }
     if (krogerToken === '') {
-        axios({
+        const response = await axios({
             method: 'post',
             url: 'https://api.kroger.com/v1/connect/oauth2/token',
             headers: {'Content-Type': 'application/x-www-form-urlencoded',
                 'Authorization' : `Basic ${btoa(KROGER_CLIENT_ID + ':' + KROGER_CLIENT_SECRET)}`},
             data: 'grant_type=client_credentials&scope=product.compact' 
         });
-    krogerToken = Response.access_token;
-    document.cookie = name + '=' + krogerToken + ';expires=' + Response.expires_in;
+    krogerToken = response.data.access_token;
+    document.cookie = name + '=' + krogerToken + ';expires=' + response.data.expires_in;
     } 
     return krogerToken;
 }
 
 const krogerSearch = (term) => {
-    const token = new Promise(krogerAuth());
-    token.then(
-        axios({
+    const token = new Promise (krogerAuth);
+    token.then ( token => {
+        return axios({
             method: 'get',
             url: `https://api.kroger.com/v1/products?filter.limit=10&filter.term=${term}`,
             headers: { 'Accept': 'application/json',
                 'Authorization': `Bearer ${token}`}   
         })
-    );
-        return Response;
+    }).then(response => response.data);
+        // return response;
 }
+console.log(krogerSearch('milk'));
 
-export default { krogerAuth, krogerSearch }
+module.exports = krogerSearch;
